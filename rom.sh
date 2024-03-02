4696a39741e2d417096c1d8ead474858a42bb361
Juleast <josephhs0815@gmail.com>
2024-03-02 15:57:46 -0500

    manifest: Update rom.sh script - Add looped renaming for when multiple device trees are cloned.

--- rom.sh
+++ rom.sh
@@ -2,25 +2,24 @@
 
 # Modify ROM prefix
 modify_rom_prefix() {
-    echo "Rom Bringup"
-    read -p "New rom prefix: " rom
+  echo "Rom Bringup"
+  read -p "New rom prefix: " rom
 
-    echo "Performing renaming..."
-    cd device/lge/${device}
-    find . -not -path "*/.*" -name "*.mk" -type f -exec sed -i "s/aosp/${rom}/g" {} +
-    mv aosp_${device}.mk ${rom}_${device}.mk
-    echo "Successfully renamed prefix to ${rom}"
+  echo "Performing renaming..."
+  cd device/lge/${device}
+  find . -not -path "*/.*" -name "*.mk" -type f -exec sed -i "s/aosp/${rom}/g" {} +
+  mv aosp_${device}.mk ${rom}_${device}.mk
+  echo "Successfully renamed prefix to ${rom}"
 
-    cd ./../../../
-    init_ksu
+  cd ./../../../
 }
 
 # Clone KernelSU submodule
 init_ksu() {
-    cd kernel/lge/sdm845
-    git submodule init
-    git submodule update
-    cd ./../../../
+  cd kernel/lge/sdm845
+  git submodule init
+  git submodule update
+  cd ./../../../
 }
 
 # Main script
@@ -29,25 +28,37 @@ echo "Select the device:"
 echo "1. judyln"
 echo "2. judypn"
 echo "3. judyp"
+echo "4. ALL"
 
-read -p "Enter your choice (1-3): " device_choice
+read -p "Enter your choice (1-4): " device_choice
 
 case ${device_choice} in
-    1)
-        device="judyln"
-        ;;
-    2)
-        device="judypn"
-        ;;
-    3)
-        device="judyp"
-        ;;
-    *)
-        echo "Invalid choice. Exiting..."
-        exit 1
-        ;;
+  1)
+    device="judyln"
+    ;;
+  2)
+    device="judypn"
+    ;;
+  3)
+    device="judyp"
+    ;;
+  4)
+    device_arr=("judyln" "judyp" "judypn")
+    ;;
+  *)
+    echo "Invalid choice. Exiting..."
+    exit 1
+    ;;
 esac
 
-modify_rom_prefix
+if [ "${device_choice}" == 4 ]; then
+  for ((i = 0; i < ${#device_arr[@]}; i++)); do
+    device="${device_arr[$i]}"
+    modify_rom_prefix
+  done
+else
+  modify_rom_prefix
+fi
+init_ksu
 
 echo "Done!"
